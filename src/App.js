e554e58f56d550f711f6b21160607f9216a0c7f8
Thiago Giffoni <giffoni@hotmail.com>
2020-09-16 14:41:32 -0300

    feat: Implementation of functionalities to include and exclude repositories.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api'
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+        setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    api
+      .post('repositories', { title: `Repository #${Date.now()}`})
+      .then(response => {
+        setRepositories([ ... repositories, response.data])
+      });
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    api
+      .delete(`repositories/${id}`)
+      .then(response => {
+        setRepositories(repositories.filter((repository) => repository.id != id));
+      });
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => 
+          <li key={repository.id}>
+            {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        )}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
